2ae64d041e6a47362c9966fad23ce5e96bb2ea02
Summer Healey <summerleigh.healey@gmail.com>
2020-05-30 18:07:39 -0400

    Added character variables for password inputs.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Defined character types. 
+
+const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+const numericalCharacters = "0123456789";
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+ 
+ 
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
