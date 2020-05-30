e7e6f0d0ddbb0d25967953febe7963d802d13875
Summer Healey <summerleigh.healey@gmail.com>
2020-05-30 18:40:25 -0400

    Prompt created to collect input for password length. If function created to validate that user input was within defined guidelines of 8 to 128 characters.

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,16 @@ const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
 const numericalCharacters = "0123456789";
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+//Prompt for the length of the password.
+let passwordLength = parseInt(prompt("Please enter desired password length. Minimum of 8 characters required. Please do not exceed 128 characters."));
+ 
+//console.log(passwordLength);
+
+//Validation that length of password is set between 8 and 128 characters.  
+if (passwordLength < 8 || passwordLength > 128) {
+  let passwordLength = alert("Invalid entry. Please choose a password length between 8 and 128 characters.");
+
  
  
 
